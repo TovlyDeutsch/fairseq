11d1856549d57aad71d347b58bbc305efa8e0f39
Tovly Deutsch <tovlydeutsch@gmail.com>
2020-07-28 21:10:14 -0400

    added preparation script

---
+++ prepare-iitb.py
@@ -0,0 +1,49 @@
+from indicnlp.tokenize import indic_tokenize
+import requests
+import tarfile
+import os
+data_dir = 'iitb'
+
+
+def download_and_extract(url, target_path):
+  if not os.path.exists(target_path):
+    response = requests.get(url, stream=True)
+    if response.status_code != 200:
+      raise ConnectionError('Download failed')
+    with open(target_path, 'wb') as f:
+      f.write(response.raw.read())
+
+  tar = tarfile.open(target_path, "r:gz")
+  tar.extractall(data_dir)
+  tar.close()
+
+
+def rewrite_tokenized(src_path, out_filename):
+  src_file = open(src_path, 'r', encoding='utf-8')
+  src_lines = src_file.read().split('\n')
+  tokenized_lines = list(map(lambda line: ' '.join(indic_tokenize.trivial_tokenize(line)), src_lines))
+  if not os.path.exists(os.path.join(data_dir, 'tokenized')):
+    os.mkdir(os.path.join(data_dir, 'tokenized'))
+  out_file = open(os.path.join(data_dir, 'tokenized', out_filename), 'w', encoding='utf-8')
+  out_file.write('\n'.join(tokenized_lines))
+
+
+def extract_and_tokenize():
+  if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
+  train_url = 'http://www.cfilt.iitb.ac.in/~moses/iitb_en_hi_parallel/iitb_corpus_download/parallel.tgz'
+  train_path = os.path.join(data_dir, 'parallel.tgz')
+  test_url = 'http://www.cfilt.iitb.ac.in/~moses/iitb_en_hi_parallel/iitb_corpus_download/dev_test.tgz'
+  test_path = os.path.join(data_dir, 'dev_test.tgz')
+  download_and_extract(train_url, train_path)
+  download_and_extract(test_url, test_path)
+  rewrite_tokenized(os.path.join(data_dir, 'parallel', 'IITB.en-hi.en'), 'IITB.en-hi.en')
+  rewrite_tokenized(os.path.join(data_dir, 'parallel', 'IITB.en-hi.hi'), 'IITB.en-hi.hi')
+  rewrite_tokenized(os.path.join(data_dir, 'dev_test', 'test.en'), 'test.en')
+  rewrite_tokenized(os.path.join(data_dir, 'dev_test', 'test.hi'), 'test.hi')
+  rewrite_tokenized(os.path.join(data_dir, 'dev_test', 'dev.en'), 'dev.en')
+  rewrite_tokenized(os.path.join(data_dir, 'dev_test', 'dev.hi'), 'dev.hi')
+
+
+if __name__ == "__main__":
+  extract_and_tokenize()
