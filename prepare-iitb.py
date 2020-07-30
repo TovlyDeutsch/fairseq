959b240b23a64d1a9543e10bb7f8e2b7a8949617
Tovly Deutsch <tovly@holylogin04.rc.fas.harvard.edu>
2020-07-30 18:47:10 -0400

    added more commands

--- prepare-iitb.py
+++ prepare-iitb.py
@@ -56,20 +56,26 @@ fairseq-preprocess --source-lang hi --target-lang en \
 """
 
 """
-CUDA_VISIBLE_DEVICES=0 fairseq-train \
+fairseq-train \
     data-bin/iitb \
     --max-tokens 4096 \
     --arch transformer --share-decoder-input-output-embed \
-    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
-    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
-    --dropout 0.3 --weight-decay 0.0001 \
-    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --optimizer adam \
+    --lr 1e-4 --min-lr 1e-9 \
+    --dropout 0.1 \
+    --criterion label_smoothed_cross_entropy \
     --eval-bleu \
-    --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+    --eval-bleu-args '{"beam": 5}' \
     --eval-bleu-detok moses \
     --eval-bleu-remove-bpe \
     --eval-bleu-print-samples \
-    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric
-    --patience 2
+    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
+    --patience 2 \
     --max-epoch 10
 """
+
+"""
+fairseq-generate data-bin/iitb \
+    --path checkpoints/checkpoint_best.pt \
+    --batch-size 128 --beam 5 --remove-bpe
+  """
