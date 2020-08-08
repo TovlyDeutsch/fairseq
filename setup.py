491ff3037cc102bfedcdd7326b9e556572c6844a
Tovly Deutsch <tovlydeutsch@gmail.com>
2020-08-08 16:12:38 -0400

    removed sacrebleu and torch deps

--- setup.py
+++ setup.py
@@ -10,34 +10,34 @@ import sys
 
 
 if sys.version_info < (3, 6):
-    sys.exit('Sorry, Python >= 3.6 is required for fairseq.')
+  sys.exit('Sorry, Python >= 3.6 is required for fairseq.')
 
 
 with open('README.md') as f:
-    readme = f.read()
+  readme = f.read()
 
 
 if sys.platform == 'darwin':
-    extra_compile_args = ['-stdlib=libc++', '-O3']
+  extra_compile_args = ['-stdlib=libc++', '-O3']
 else:
-    extra_compile_args = ['-std=c++11', '-O3']
+  extra_compile_args = ['-std=c++11', '-O3']
 
 
 class NumpyExtension(Extension):
-    """Source: https://stackoverflow.com/a/54128391"""
+  """Source: https://stackoverflow.com/a/54128391"""
 
-    def __init__(self, *args, **kwargs):
-        self.__include_dirs = []
-        super().__init__(*args, **kwargs)
+  def __init__(self, *args, **kwargs):
+    self.__include_dirs = []
+    super().__init__(*args, **kwargs)
 
-    @property
-    def include_dirs(self):
-        import numpy
-        return self.__include_dirs + [numpy.get_include()]
+  @property
+  def include_dirs(self):
+    import numpy
+    return self.__include_dirs + [numpy.get_include()]
 
-    @include_dirs.setter
-    def include_dirs(self, dirs):
-        self.__include_dirs = dirs
+  @include_dirs.setter
+  def include_dirs(self, dirs):
+    self.__include_dirs = dirs
 
 
 extensions = [
@@ -68,51 +68,51 @@ cmdclass = {}
 
 
 try:
-    # torch is not available when generating docs
-    from torch.utils import cpp_extension
+  # torch is not available when generating docs
+  from torch.utils import cpp_extension
+  extensions.extend([
+      cpp_extension.CppExtension(
+          'fairseq.libnat',
+          sources=[
+              'fairseq/clib/libnat/edit_dist.cpp',
+          ],
+      )
+  ])
+
+  if 'CUDA_HOME' in os.environ:
     extensions.extend([
         cpp_extension.CppExtension(
-            'fairseq.libnat',
+            'fairseq.libnat_cuda',
             sources=[
-                'fairseq/clib/libnat/edit_dist.cpp',
+                'fairseq/clib/libnat_cuda/edit_dist.cu',
+                'fairseq/clib/libnat_cuda/binding.cpp'
             ],
-        )
-    ])
-
-    if 'CUDA_HOME' in os.environ:
-        extensions.extend([
-            cpp_extension.CppExtension(
-                'fairseq.libnat_cuda',
-                sources=[
-                    'fairseq/clib/libnat_cuda/edit_dist.cu',
-                    'fairseq/clib/libnat_cuda/binding.cpp'
-                ],
-            )])
-    cmdclass['build_ext'] = cpp_extension.BuildExtension
+        )])
+  cmdclass['build_ext'] = cpp_extension.BuildExtension
 
 except ImportError:
-    pass
+  pass
 
 
 if 'READTHEDOCS' in os.environ:
-    # don't build extensions when generating docs
-    extensions = []
-    if 'build_ext' in cmdclass:
-        del cmdclass['build_ext']
+  # don't build extensions when generating docs
+  extensions = []
+  if 'build_ext' in cmdclass:
+    del cmdclass['build_ext']
 
-    # use CPU build of PyTorch
-    dependency_links = [
-        'https://download.pytorch.org/whl/cpu/torch-1.3.0%2Bcpu-cp36-cp36m-linux_x86_64.whl'
-    ]
+  # use CPU build of PyTorch
+  dependency_links = [
+      'https://download.pytorch.org/whl/cpu/torch-1.3.0%2Bcpu-cp36-cp36m-linux_x86_64.whl'
+  ]
 else:
-    dependency_links = []
+  dependency_links = []
 
 
 if 'clean' in sys.argv[1:]:
-    # Source: https://bit.ly/2NLVsgE
-    print("deleting Cython files...")
-    import subprocess
-    subprocess.run(['rm -f fairseq/*.so fairseq/**/*.so fairseq/*.pyd fairseq/**/*.pyd'], shell=True)
+  # Source: https://bit.ly/2NLVsgE
+  print("deleting Cython files...")
+  import subprocess
+  subprocess.run(['rm -f fairseq/*.so fairseq/**/*.so fairseq/*.pyd fairseq/**/*.pyd'], shell=True)
 
 
 setup(
@@ -138,8 +138,8 @@ setup(
         'cython',
         'numpy',
         'regex',
-        'sacrebleu',
-        'torch',
+        # 'sacrebleu',
+        # 'torch',
         'tqdm',
     ],
     dependency_links=dependency_links,
